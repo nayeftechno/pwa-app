69622c76a8c02894ba94a69dc6478c04ff7f8533
nayeftechno <nayeftechno@gmail.com>
2022-03-12 12:53:49 +0300

    Fix app header color

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
+//check service worker support
+//register service worker with browser
+//to implement app like behaviour into our website
 if ("serviceWorker" in navigator) {
+  //navigator object provides information about the web browser and its capabilities.
   navigator.serviceWorker
     .register("/sw.js")
-    .then((reg) => console.log("service worker registered", reg))
-    .catch((err) => console.log("service worker not registered", err));
+    .then((reg) => {
+      console.log("service worker registered");
+      console.log(reg);
+    })
+    .catch((err) => {
+      console.log("service worker not registered", err);
+    });
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-// install event
+//self refers to servive worker itself
+
+//add install event
+//listen to install event
 self.addEventListener("install", (evt) => {
   console.log("service worker installed");
+});
+//add activate event
+//listen to activate event
+self.addEventListener("activate", (evt) => {
+  console.log("service worker activated");
 });
